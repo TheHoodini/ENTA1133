cdd09c7a47b68bfc07846daefb0350e92fdbfb44
Shomu10216282 <garciasalasjd@gmail.com>
2025-09-05 20:53:45 -0700

    feat: add coding project

---
+++ GD14_1133_Lab1_garcia_juan/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GD14_1133_Lab1_garcia_juan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Worldaa!");
+        }
+    }
+}
