c3f214dc31f5f31dd6f2a5e19a2eb2cb28fbeab4
Shomu10216282 <garciasalasjd@gmail.com>
2025-09-18 19:09:23 -0700

    fix: change die rolls output

--- DiceGame/DieRoller.cs
+++ DiceGame/DieRoller.cs
@@ -15,26 +15,26 @@ namespace GD14_1133_A1_JuanDiego_DiceGame
         {
             int result = 0;
             int rollNumber;
-            Console.WriteLine("Rolling the die...");
+            Console.WriteLine("Rolling the d"+ numberOfSidesRoll +"...");
             Thread.Sleep(800);
             rollNumber = random.Next(1, numberOfSidesRoll + 1);
-            Console.WriteLine("Your first roll was a " + rollNumber + "!");
+            Console.WriteLine("First roll:  " + rollNumber);
             result += rollNumber;
             rollNumber = random.Next(1, numberOfSidesRoll + 1);
             Thread.Sleep(800);
-            Console.WriteLine("Your second roll was a " + rollNumber + "!");
+            Console.WriteLine("Second roll: " + rollNumber);
             result += rollNumber;
             rollNumber = random.Next(1, numberOfSidesRoll + 1);
             Thread.Sleep(800);
-            Console.WriteLine("Your third roll was a " + rollNumber + "!");
+            Console.WriteLine("Third roll:  " + rollNumber);
             result += rollNumber;
             rollNumber = random.Next(1, numberOfSidesRoll + 1);
             Thread.Sleep(800);
-            Console.WriteLine("Your fourth roll was a " + rollNumber + "!");
+            Console.WriteLine("Fourth roll: " + rollNumber);
             result += rollNumber;
             Thread.Sleep(800);
 
-            Console.WriteLine("Your total: "+ result);
+            Console.WriteLine("-----------------\nYour total:  "+ result);
         }
     }
 }
