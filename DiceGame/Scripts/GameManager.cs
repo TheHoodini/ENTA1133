5b799317c4b42a0806fba648d26e3213f4bb6408
Juan Diego García <salasjuan@uninorte.edu.co>
2025-09-19 01:21:24 -0700

    feat: input validation

--- DiceGame/Scripts/GameManager.cs
+++ DiceGame/Scripts/GameManager.cs
@@ -17,35 +17,42 @@ namespace GD14_1133_A1_JuanDiego_DiceGame.Scripts
             Console.WriteLine("Welcome "+ playerName +" to the Dice Game!\n"+ todayDate +"\n");
             Thread.Sleep(800);
 
-            // Ask for the type of die
+            // Ask for the type of die and validate answer
             Console.Write("Enter the number of sides your die will have: ");
             int numberOfSides = int.Parse(Console.ReadLine());
+            if (numberOfSides <= 1) 
+            { 
+                Console.WriteLine("The number of sides must be a positive number greater than 1!");
+            } 
+            else 
+            {
+                // Create a DieRoller and call the Roll method
+                DieRoller dieRoller = new DieRoller(numberOfSides);
+                Thread.Sleep(500);
+                dieRoller.Roll();
 
-            // Create a DieRoller and call the Roll method
-            DieRoller dieRoller = new DieRoller(numberOfSides);
-            Thread.Sleep(500);
-            dieRoller.Roll();
+                // Aritmetic operations explanation
+                Thread.Sleep(1000);
+                Console.WriteLine("\nHere is an explanation about the different types of arithmetic operators in programming:");
+                Thread.Sleep(1000);
+                Console.WriteLine("Addition (+): Adds two values. ex: 4 + 7 = 11");
+                Thread.Sleep(1000);
+                Console.WriteLine("Subtraction (-): Subtracts one value from another. ex: 10 - 3 = 7");
+                Thread.Sleep(1000);
+                Console.WriteLine("Division (/): Divides one value by another. ex: 20 / 4 = 5");
+                Thread.Sleep(1000);
+                Console.WriteLine("Multiplication (*): Multiplies two values. ex: 5 * 6 = 30");
+                Thread.Sleep(1000);
+                Console.WriteLine("Increment (++): increases the value of the variable by 1. ex: x = 5 -> x++ -> x = 6");
+                Thread.Sleep(1000);
+                Console.WriteLine("Decrement (--): decreases the value of the variable by 1. ex: x = 5 -> x-- -> x = 4");
+                Thread.Sleep(1000);
+                Console.WriteLine("Modulus (%): Returns the remainder of a division operation. ex: 10 % 3 = 1");
 
-            // Aritmetic operations explanation
-            Thread.Sleep(1000);
-            Console.WriteLine("\nHere is an explanation about the different types of arithmetic operators in programming:");
-            Thread.Sleep(1000);
-            Console.WriteLine("Addition (+): Adds two values. ex: 4 + 7 = 11");
-            Thread.Sleep(1000);
-            Console.WriteLine("Subtraction (-): Subtracts one value from another. ex: 10 - 3 = 7");
-            Thread.Sleep(1000);
-            Console.WriteLine("Division (/): Divides one value by another. ex: 20 / 4 = 5");
-            Thread.Sleep(1000);
-            Console.WriteLine("Multiplication (*): Multiplies two values. ex: 5 * 6 = 30");
-            Thread.Sleep(1000);
-            Console.WriteLine("Increment (++): increases the value of the variable by 1. ex: x = 5 -> x++ -> x = 6");
-            Thread.Sleep(1000);
-            Console.WriteLine("Decrement (--): decreases the value of the variable by 1. ex: x = 5 -> x-- -> x = 4");
-            Thread.Sleep(1000);
-            Console.WriteLine("Modulus (%): Returns the remainder of a division operation. ex: 10 % 3 = 1");
-
-            Thread.Sleep(1000);
-            Console.WriteLine("\nThanks " + playerName + " for playing! Goodbye!");
+                Thread.Sleep(1000);
+                Console.WriteLine("\nThanks " + playerName + " for playing! Goodbye!");
+            }
+            
         }
     }
 }
